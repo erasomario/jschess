acfb7dfc949db358e69cc49b69b29c56e7598140
Mario Eraso <erasomario@qualisys.com.co>
2021-08-08 22:00:22 -0500

    makeGamelistDto made independant of other makers

--- model/gamelist-dto/gamelist-dto-model.js
+++ model/gamelist-dto/gamelist-dto-model.js
@@ -1,12 +1,15 @@
 const Joi = require('joi');
 const { validate } = require('../../utils/Validation');
+const { findUserById } = require('../user/user-logic');
 
-const gameSchema = Joi.object({
+const gamelistDtoSchema = Joi.object({
     id: Joi.string().required(),
     whiteId: Joi.string(),
     blackId: Joi.string(),
     whiteName: Joi.string(),
     blackName: Joi.string(),
+    blackHasPicture: Joi.boolean(),
+    whiteHasPicture: Joi.boolean(),
     createdAt: Joi.date().required().default(Date.now),
     lastMovAt: Joi.date(),
     result: Joi.string().valid('w', 'b', 'd'),
@@ -17,15 +20,36 @@ const gameSchema = Joi.object({
     createdBy: Joi.string().valid('w', 'b'),
 })
 
-const makeGamelistDto = (obj) => {
+const makeGamelistDto = async game => {
+    const obj = { ...game }
+
     if (!obj.whiteId) {
         obj.whiteName = "Robot"
+        obj.whiteHasPicture = true
     }
 
     if (!obj.blackId) {
         obj.blackName = "Robot"
+        obj.blackHasPicture = true
     }
-    return validate(gameSchema, obj)
+
+    if (!obj.whiteName && obj.whiteId) {
+        const white = await findUserById(obj.whiteId)
+        obj.whiteName = white.username
+        obj.whiteHasPicture = white.hasPicture
+    }
+    if (!obj.blackName && obj.blackId) {
+        const black = await findUserById(obj.blackId)
+        obj.blackName = black.username
+        obj.blackHasPicture = black.hasPicture
+    }
+
+    delete obj.requestedColor
+    delete obj.movs
+    delete obj.endType
+    obj.turn = game.movs.length
+    
+    return validate(gamelistDtoSchema, obj)
 }
 
 module.exports = makeGamelistDto
